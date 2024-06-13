e45f573470fc8b68de416ba5b96653d4d7ed7c89
Gabriel <gabrielmmve13@gmail.com>
2024-06-12 21:31:11 -0300

    implementação do layout da administração

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -8,40 +8,25 @@ use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    // traz todos os registros
+    
     public function index()
     {   
-        // estou dizendo que quero que traga tudo que está no DB, isso é igual a um SELECT *
-        // $usuarios = User::all();
-
-        /* 
-        * utilizando where
-        * dizendo que quero que retornde ONDE, no campo email, ele seja igual a 'bquintana@example.net'
-        * -> get() é obrigatório
-        */
-        $usuarios = User::where('email', 'bquintana@example.net') ->get();
-
-        /* 
-        * dd variavel do laravel que retorna objetos - utilizado para deputrar e ver se tem algum erro 
-        * Aqui, a variável $usuarios armazena todos os registros do modelo User, e dd($usuarios) é usado para depuração, exibindo o conteúdo da variável.    
-        * pra ver se está retornando tudo certo tambem
-        */
-        dd($usuarios);
+        return view('admin.usuarios.index');
     }
 
     
     public function create()
     {
-        //
+        
     }
 
   
     public function store(Request $request)
     {
-        //
+        
     }
 
-    // traz apenas um registro
+   
     public function show(string $id)
     {
         // findOrFail, irá buscar o id do usuário, e SE encontrar algum erro, irá retornar o erro 404 - isso equivale a um SELECT id = FROM usuarios
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,15 @@
 
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 // [SiteController::class, "home"] - dentro de SiteController, quero carregar o método home()
 Route::get('/',[SiteController::class, "home"]);
-// '/sobre-nos' - o que o usuário vai digitar na URL, então, letra minúscula
+// '/sobre-nos' - o que o usuário vai digitar na URL, então, letra minúscula - trazendo a view sobre-nos.blade.php
 Route::get('/sobre-nos',[SiteController::class, "sobreNos"]);
-
+// trazendo a view contato atraves do controller
 Route::get('/contato',[SiteController::class, "contato"]);
 
 // get pois quero mostrar algo na tela
@@ -17,3 +18,7 @@ Route::get('/contato',[SiteController::class, "contato"]);
 Route::get('/admin/usuarios', [UsuarioController::class, "index"]);
 // se estiver com o parametro id (sem $ pois é um parametro) irá chamar o método show, que mostra um único usuário (este será um sub-arquivo de /admin/usuarios) 
 Route::get('/admin/usuarios/{id}', [UsuarioController::class, "show"]);
+
+Route::get('/admin/dashboard', [DashboardController::class, "dashboard"]);
+
+Route::get('/admin/index',[UsuarioController::class, "index"] );
