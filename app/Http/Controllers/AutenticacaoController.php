df9503627d0f2e42d127a78b60126bbd11bedcbc
Gabriel Erick <seu-email@exemplo.com>
2024-08-19 21:33:49 -0300

    criando processo de autenticação e login

---
+++ app/Http/Controllers/AutenticacaoController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AutenticacaoController extends Controller
+{
+    public function formLogin()
+    {
+        return view("admin.autenticacao.login");
+    }
+
+    // request contém todos os dados enviados na requisição http (url), como dados de formulário parâmetros da url, etc
+    public function login(Request $request)
+    {
+        // request->validate é utilizado para garantir que os dados recebidos atendam a certos critérios antes de continuar com o processamento, se a validação falhar, será redirecionado para a página anterior com mensagens de erro
+        // valida o usuário e joga as informações na váriavel $dadosUsuario (um array)
+        $dadosUsuario = $request->validate([
+            // [required, email] tem que ser um email valido, não pode ser deixado em branco
+            "email" => ["required", "email"],
+            // campo password não pode ser deixado em branco
+            "password" => "required"
+        ]);
+        /*
+        * Auth está ligado diretamente com o model de user, model usuários e utilizará suas informações 
+        * Auth:;attempt() - se as credenciais forem validas (ou seja, se existir um usuário no banco de dados com essas credenciais), Auth::attempt() retornará true e o usuário será autenticado
+        */
+        if (Auth::attempt($dadosUsuario)) {
+            // após a atutenticação bem sucedida a sesão do usuário é regenerada (o que ajuda a proteger contra ataques de fixação de sessão, onde poderia ser explorado a sessão antiga do usuário)
+            $request->session()->regenerate();
+            /*
+             * redirect() - loga e volta para a ultima rota protegida que ele tentou acessar
+             * intended("/admin/dashboard"), caso ele não tentou acessar nenhuma rota protegida (como algo de admin, que alguem que não esta logado não pode acessar), ele irá por padrão nesta rota
+              */
+            return redirect()->intended("/admin/dashboard");
+        }
+        /* 
+        * se a tentativa de autenticação falhar (se as credenciais não forem válidas) este bloco será executado
+        * manda de volta para a ultima página em que estava com redirect->back()
+        * withErrors() adiciona uma mensagem de erro à sessão, que pode ser exibida na view para informar ao usuário que o email ou a senha são inválidos. Neste caso, a mensagem associada ao campo "email" é "Usuário ou Senha inválida"
+        */
+        return redirect()->back()->withErrors(["email" => "Usuário ou Senha inválida"]);
+    }
+
+    public function logout(Request $request)
+    {
+        // realizando logout/apagando do db
+        Auth::logout();
+        // invalidando a sesão
+        $request->session()->invalidate();
+        // invalidando token
+        $request->session()->regenerateToken();
+
+        return redirect("/");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,39 +5,55 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\ServicoController;
+use App\Http\Controllers\AutenticacaoController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [SiteController::class, "home"])->name("site.home");
+Route::get('/sobre-nos', [SiteController::class, "sobreNos"])->name("site.sobrenos");
+Route::get('/contato', [SiteController::class, "contato"])->name("site.contato");
 
-Route::get('/',[SiteController::class, "home"])->name("site.home");
-Route::get('/sobre-nos',[SiteController::class, "sobreNos"])->name("site.sobrenos");
-Route::get('/contato',[SiteController::class, "contato"])->name("site.contato");
+/*
+* travando as rotas
+* fazendo com que, se o usuário não estiver logado, ele não tem acesso a essas rotas 
+*/
+Route::middleware(["auth"])->group(function () {
 
-//Rotas da Seção Usuário
-Route::get('/admin/usuarios',[UsuarioController::class, "index"])->name("usuario.index");
-Route::get('/admin/usuarios/cadastrar', [UsuarioController::class, "create"])->name("usuario.create");
-Route::get('/admin/usuarios/editar/{id}', [UsuarioController::class, "edit"])->name("usuario.edit");
-Route::get('/admin/usuarios/visualizar/{id}', [UsuarioController::class, "show"])->name("usuario.show");
-Route::post('/admin/usuarios/cadastrar/salvar', [UsuarioController::class, "store"])->name("usuario.store");
-Route::put('/admin/usuarios/atualizar/{id}', [UsuarioController::class, "update"])->name("usuario.update");
-Route::delete('/admin/usuarios/deletar/{id}', [UsuarioController::class, "destroy"])->name("usuario.destroy");
+    //Rotas da Seção Usuário
+    Route::get('/admin/usuarios', [UsuarioController::class, "index"])->name("usuario.index");
+    Route::get('/admin/usuarios/cadastrar', [UsuarioController::class, "create"])->name("usuario.create");
+    Route::get('/admin/usuarios/editar/{id}', [UsuarioController::class, "edit"])->name("usuario.edit");
+    Route::get('/admin/usuarios/visualizar/{id}', [UsuarioController::class, "show"])->name("usuario.show");
+    Route::post('/admin/usuarios/cadastrar/salvar', [UsuarioController::class, "store"])->name("usuario.store");
+    Route::put('/admin/usuarios/atualizar/{id}', [UsuarioController::class, "update"])->name("usuario.update");
+    Route::delete('/admin/usuarios/deletar/{id}', [UsuarioController::class, "destroy"])->name("usuario.destroy");
 
-//Rotas da Seção Categorias
-Route::get('/admin/categorias', [CategoriaController::class, "index"])->name("categoria.index");
-Route::get('/admin/categorias/cadastrar', [CategoriaController::class, "create"])->name("categoria.create");
-Route::get('/admin/categorias/editar/{id}', [CategoriaController::class, "edit"])->name("categoria.edit");
-Route::get('/admin/categorias/visualizar/{id}', [CategoriaController::class, "show"])->name("categoria.show");
-Route::post('/admin/categorias/cadastrar/salvar', [CategoriaController::class, "store"])->name("categoria.store");
-Route::put('/admin/categorias/atualizar/{id}', [CategoriaController::class, "update"])->name("categoria.update");
-Route::delete('/admin/categorias/deletar/{id}', [CategoriaController::class, "destroy"])->name("categoria.destroy");
+    //Rotas da Seção Categorias
+    Route::get('/admin/categorias', [CategoriaController::class, "index"])->name("categoria.index");
+    Route::get('/admin/categorias/cadastrar', [CategoriaController::class, "create"])->name("categoria.create");
+    Route::get('/admin/categorias/editar/{id}', [CategoriaController::class, "edit"])->name("categoria.edit");
+    Route::get('/admin/categorias/visualizar/{id}', [CategoriaController::class, "show"])->name("categoria.show");
+    Route::post('/admin/categorias/cadastrar/salvar', [CategoriaController::class, "store"])->name("categoria.store");
+    Route::put('/admin/categorias/atualizar/{id}', [CategoriaController::class, "update"])->name("categoria.update");
+    Route::delete('/admin/categorias/deletar/{id}', [CategoriaController::class, "destroy"])->name("categoria.destroy");
 
-//Rotas da Seção DashBoard
-Route::get('/admin/dashboard',[DashboardController::class, "dashboard"])->name("dashboard");
+    //Rotas da Seção DashBoard
+    Route::get('/admin/dashboard', [DashboardController::class, "dashboard"])->name("dashboard");
 
-//Rotas da Seção Serviços
-Route::get('/admin/servicos', [ServicoController::class, "index"])->name("servico.index");
-Route::get('/admin/servicos/cadastrar', [ServicoController::class, "create"])->name("servico.create");
-Route::get('/admin/servicos/visualizar/{id}', [ServicoController::class, "show"])->name("servico.show");
-Route::get('/admin/servicos/editar/{id}', [ServicoController::class, "edit"])->name("servico.edit");
-Route::delete('/admin/servicos/deletar/{id}', [ServicoController::class, "destroy"])->name("servico.destroy");
-Route::post('/admin/servicos/cadastrar/salvar', [ServicoController::class, "store"])->name("servico.store");
-Route::put('/admin/servicos/atualizar/{id}', [ServicoController::class, "update"])->name("servico.update");
+    //Rotas da Seção Serviços
+    Route::get('/admin/servicos', [ServicoController::class, "index"])->name("servico.index");
+    Route::get('/admin/servicos/cadastrar', [ServicoController::class, "create"])->name("servico.create");
+    Route::get('/admin/servicos/visualizar/{id}', [ServicoController::class, "show"])->name("servico.show");
+    Route::get('/admin/servicos/editar/{id}', [ServicoController::class, "edit"])->name("servico.edit");
+    Route::delete('/admin/servicos/deletar/{id}', [ServicoController::class, "destroy"])->name("servico.destroy");
+    Route::post('/admin/servicos/cadastrar/salvar', [ServicoController::class, "store"])->name("servico.store");
+    Route::put('/admin/servicos/atualizar/{id}', [ServicoController::class, "update"])->name("servico.update");
+});
+/*
+* se for submetido por get, vai nessa rota
+* middleware("guest") é para garantir que ela só possa ser acessada se o usuário for um visitante, ou seja, para usuários não autenticados (visitantes, ou guests)
+* se o usuário já estiver autenticado, o middleware "guest" redireciona para outra página, geralmente dashboard ou página inicial, evitando que usuários logados acessem a página login
+*/
+route::get("/login", [AutenticacaoController::class, "formLogin"])->name("login.Form")->middleware("guest");
+// se for submetido por post, vai nessa rota
+route::post("/login", [AutenticacaoController::class, "login"])->name("login");
+route::get("/logout", [AutenticacaoController::class, "logout"])->name("logout");
